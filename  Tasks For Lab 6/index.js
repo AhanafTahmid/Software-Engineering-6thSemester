82f95db98c61bcde04d258600f2393b099114883
AhanafTahmid <ahanaftahmid55@gmail.com>
2025-05-25 01:53:18 +0600

    Lab 5,6 Completed

---  Tasks For Lab 6/index.js
+++  Tasks For Lab 6/index.js
@@ -1,52 +1,72 @@
 const express = require('express');
 const fs = require('fs');
-const data = require('./MOCK_DATA.json');
 const app = express();
+const mongoose = require('mongoose');
 
-//Rest API for Sample Companies (Having ID, Compnay Name, email)
+//connect mongoDB
+const str = 'mongodb+srv://ahanaf627:uU8tjDeyvY80Jlmo@cluster0.jjc6mcn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+mongoose.connect(str)
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((err) => {
+    console.log("Mongo Error",err);
+})
+
+//mongodb pass: uU8tjDeyvY80Jlmo
+
+//Schema
+const companySchema = new mongoose.Schema({
+    company_name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    location:{
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+})
+
+//Model
+const Company = mongoose.model('Company', companySchema);
 
 app.use(express.urlencoded());
-app.get('/items/companies', (req, res) => {
-    //return res.json(data);
-    const html = ` 
-    <ul> ${ data.map(u=> `<li>${u.company_name}</li>`).join("") } </ul>
-    `
-    res.send(html)
+
+app.get('/items', async (req, res) => {
+    const allcompanies = await Company.find({});
+    //console.log(allcompanies);
+    return res.json(allcompanies);
 });
 
-app.get('/items', (req, res) => {
-    return res.json(data);
-});
-
-app.post('/items', (req, res) => {
+app.post('/items', async (req, res) => {
     const body = req.body;
-    data.push({id: data.length + 1, ...body});
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(data), () => {});
-    return res.status(201).json({status: "success", id:data.length});
+    //console.log(body);
+    if( !body || !body.company_name || !body.email || !body.location ) return res.status(400).json({status: "fail"});
+    
+    const newentry = await Company.create({
+        company_name: body.company_name,
+        email: body.email,
+        location: body.location
+    });
+    console.log("New Entry Added: ", newentry);
+    return res.status(201).json({status: "success"});
 });
 
-app.put('/items/:id', (req, res) => {
-    const body = req.body;//what I am sending in api, company name
-    const id = req.params.id;
-    const idx = data.findIndex(item => item.id == id);
-    // console.log(idx)
-    if( idx == -1) return res.status(404).json({status: "fail"});
-    else{
-        data[idx] = { ...data[idx], ...body };
-        fs.writeFile("./MOCK_DATA.json", JSON.stringify(data), () => {});
-        return res.json({status: "success"});
-    }
+app.put('/items/:id', async (req, res) => {
+    await Company.findByIdAndUpdate(req.params.id , {company_name: "changed_company"});
+    return res.status(201).json({status: "success"});
 });
 
-app.delete('/items/:id', (req, res) => {
-    const id = req.params.id;
-    const idx = data.findIndex(item => item.id == id);
-    if( idx == -1) return res.status(404).json({status: "fail"});
-    else{
-        data.splice(idx,1);
-        fs.writeFile("./MOCK_DATA.json", JSON.stringify(data), () => {});
-        res.json({ message: `Company with id ${id} deleted.` });
-    }
+app.delete('/items/:id', async (req, res) => {
+    await Company.findByIdAndDelete(req.params.id);
+    return res.status(201).json({status: "success"});
 });
 
 app.listen(3000, console.log("Running on port 3000"));
--- readme.md
+++ readme.md
@@ -9,6 +9,6 @@ https://docs.google.com/document/d/1BsuMsIDTF-o1AcpzUXo53ugDBOYQayo2sqeQbX4wFEU/
 ## Lab Report 4:
 https://docs.google.com/document/d/1Yxif14Am93dwwDmfCAyWn_F8x5ZrACSoAKxpSpnogAk/edit?usp=sharing
 ## Lab Report 5:
-
+https://docs.google.com/document/d/1a02c2m4RPOG_nOCUEuZVdKx78t8-kEDztUNAr4KIQPI/edit?usp=sharing
 ## Lab Report 6:
-
+https://docs.google.com/document/d/1DCE0n783rLP9G9KmKB1BeX66HzHwAT6Zlp_JPB0hqbA/edit?usp=sharing
